bbbc835c72f073a40080df3a1dc0bbd8492c9bee
saran <saran@fintel.ai>
2020-04-20 16:56:33 +0530

    Initial Commit

---
+++ ChatKit/ChatKit.h
@@ -0,0 +1,19 @@
+//
+//  ChatKit.h
+//  ChatKit
+//
+//  Created by saran on 20/04/20.
+//  Copyright © 2020 saran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatKit.
+FOUNDATION_EXPORT double ChatKitVersionNumber;
+
+//! Project version string for ChatKit.
+FOUNDATION_EXPORT const unsigned char ChatKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatKit/PublicHeader.h>
+
+
